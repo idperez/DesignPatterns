9738aca90952a7b7e62808c77e6cdc400b7e32c7
isidro <idperez@asu.edu>
2017-03-26 15:13:22 -0700

    Template Method Pattern

---
+++ src/templateMethod/CaffeineBeverage.java
@@ -0,0 +1,23 @@
+package templateMethod;
+
+public abstract class CaffeineBeverage {
+
+    final void prepareRecidpe() {
+        boilWater();
+        brew();
+        pourInCup();
+        addCondiments();
+    }
+
+    abstract void brew();
+
+    abstract void addCondiments();
+
+    void boilWater() {
+        System.out.println("Boiling water");
+    }
+
+    void pourInCup() {
+        System.out.println("Pouring into cup");
+    }
+}
